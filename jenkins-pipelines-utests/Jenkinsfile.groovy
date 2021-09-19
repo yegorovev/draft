2a75793e42620d71f6a421b121e7b810e430f297
yegorovev <evgenyyegorov@gmail.com>
2021-09-19 19:33:05 +0300

    Test1

---
+++ jenkins-pipelines-utests/Jenkinsfile.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Utests') {
+            steps {
+                dir('jenkins-pipelines-utests') {
+                    sh 'mvn -B -Dmaven.test.failure.ignore clean verify'
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                }                
+            }
+        }
+    }
+
+    post { 
+        always { 
+            cleanWs()
+        }
+    }
+}
