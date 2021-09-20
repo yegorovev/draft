b3ba08aef58b560a1c280512dc446fca5ead32b4
yegorovev <evgenyyegorov@gmail.com>
2021-09-20 22:08:11 +0300

    Update Jenkinsfile.groovy

--- jenkins-multibranch/Jenkinsfile.groovy
+++ jenkins-multibranch/Jenkinsfile.groovy
@@ -4,11 +4,11 @@ pipeline {
     stages {
         stage('Utests') {
             steps {
-                println "$BRANCH_NAME"
+                sh 'echo $BRANCH_NAME'
                 dir('jenkins-pipelines-utests') {
                     sh 'mvn -B -Dmaven.test.failure.ignore clean verify'
                     junit '**/target/surefire-reports/TEST-*.xml'
-                }                
+                }
             }
         }
     }
