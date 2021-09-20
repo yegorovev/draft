02f70ceb2ce5a6f8d02f753252115c8d75a0cbe2
yegorovev <evgenyyegorov@gmail.com>
2021-09-20 21:59:57 +0300

    test

---
+++ jenkins-multibranch/Jenkinsfile.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent {label 'docker-maven'}
+
+    stages {
+        stage('Utests') {
+            steps {
+                println "$BRANCH_NAME"
+                dir('jenkins-pipelines-utests') {
+                    sh 'mvn -B -Dmaven.test.failure.ignore clean verify'
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                }                
+            }
+        }
+    }
+
+    post { 
+        always { 
+            cleanWs()
+        }
+    }
+}
