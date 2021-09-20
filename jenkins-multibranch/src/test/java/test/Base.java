b6a5b469b5e352d43534b1a81c28c98ea087609a
yegorovev <91023323+yegorovev@users.noreply.github.com>
2021-09-20 22:56:46 +0300

    Update Base.java

--- jenkins-multibranch/src/test/java/test/Base.java
+++ jenkins-multibranch/src/test/java/test/Base.java
@@ -32,7 +32,7 @@ class Base {
     protected void run() {
         double r = Math.random();
         if (r < 0.1) {
-            fail("oops");
+            fail("oops!!!");
         } else if (r < 0.2) {
             throw new AssumptionViolatedException("skipping");
         }
